fc2f059b235bb078572329df67807581c51eee45
Romraev <romraev@yandex.ru>
2023-04-07 14:35:45 +0300

    first task

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Подготовил Раевский Р.Ю.
+
+// Task 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+/* void Numbers(int N)
+{
+    Console.Write(N+" ");
+    if(N>1)  Numbers(N-1);
+}
+
+Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Numbers(num);
+*/
